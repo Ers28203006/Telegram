7da83fe8e04e1baa42b61cfa1285e5b80bb00f9a
Ers28203006 <ersyn_sultanbaev@mail.ru>
2019-05-26 15:02:19 +0600

    Добавьте файлы проекта.

---
+++ Telegram/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Telegram
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ModeButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (modeButton.Content == FindResource("Day"))
+            {
+                modeButton.Content = FindResource("Night");
+                window.Background = Brushes.White;
+
+                var bc = new BrushConverter();
+                menu.Background = (Brush)bc.ConvertFrom("#47779a");
+                listBox.Background = Brushes.White;
+            }
+            else
+            {
+                modeButton.Content = FindResource("Day");
+                window.Background = Brushes.LightGray;
+                menu.Background = Brushes.Gray;
+                listBox.Background = Brushes.LightGray;
+            }
+        }
+    }
+}
